513ad0923d687fd8b812c12472346c1abf5bc380
ebukaakara <the1stnigerian@gmail.com>
2020-09-27 17:32:31 +0100

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import Services from "../components/Services";
+import FeaturedRooms from "../components/FeaturedRooms";
+const home = () => {
+  return (
+    <>
+      <Hero>
+        <Banner
+          title="Luxurious rooms"
+          subtitle = "Deluxe rooms starting at ₦45,000"
+        >
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+      <FeaturedRooms />
+    </>
+  );
+};
+
+export default home;
